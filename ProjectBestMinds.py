771139712a4743579b41c5f6fe2d130b50dd7258
Yan Araújo <94764518+Yan-Araujo@users.noreply.github.com>
2022-12-03 23:18:08 -0300

    Update ProjectBestMinds.py

--- ProjectBestMinds.py
+++ ProjectBestMinds.py
@@ -81,28 +81,3 @@ def refazer_processo():
 if __name__ == "__main__":
     sistema_principal()
 
-
-# arq = open('listaDeUsuarios.txt', 'a')
-# print('Olá, aqui você pode adicionar uma nova conta!')
-# nome_usuario = input('Digite o nome de usuário: ')
-#
-# arq.write('{}\n'.format(nome_usuario))
-# '''
-# Adição da constante \n new line
-# Uma vez que write não o adiciona automaticamente
-# '''
-#
-# print('Cadastro realizado com sucesso!\n')
-# arq.close() #O arquivo é fechado do modo de adição para ser aberto
-#             #posteriormente no modo de leitura
-#
-# arq = open('listaDeUsuarios.txt') #abrindo no modo de leitura
-# print('Efetue o seu login')
-# nome_login = input('Digite o seu nome de usuario: ')
-#
-# registrados = arq.readlines()
-# if nome_login + '\n' in registrados:
-#     print('Bem vindo(a), {}!'.format(nome_login))
-# else:
-#     print('Você deve ter digitado seu nome de usuario errado, por favor verifique.')
-# arq.close()
